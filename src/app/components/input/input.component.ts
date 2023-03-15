b2533c43558bf4986ec18790264bcfdfabb8340e
deilor101 <matehenrique20@hotmail.com>
2023-03-15 16:18:39 -0300

    parte 02 finalizada

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,27 @@
+import { ListaDeCompraService } from './../../service/lista-de-compra.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+
+  valorItem! : string;
+
+  constructor(
+    private listaService: ListaDeCompraService
+  ) { }
+
+  ngOnInit(): void { }
+
+  adicionarItem() {
+    this.listaService.adicionarItemNaLista(this.valorItem);
+    this.limparCampo();
+  }
+
+  limparCampo(){
+    this.valorItem = '';
+  }
+}
